2c4e9ad3b15a24eb7d6e4cd0f732b5c6bce9a976
Sim Ng <sysim19@gmail.com>
2018-06-05 21:40:36 +0800

    update portfolio

--- js/main.js
+++ js/main.js
@@ -43,44 +43,37 @@ $( document ).ready(function() {
   };
 
   scrollAnchor();
-
-
-}); // d
-
-
-// Ajak call
-
-
-
-//end of code
-
-
-// $(document).ready(function() {
   
-//   var scrollLink = $('.scroll');
-  
-//   // Smooth scrolling
-//   scrollLink.click(function(e) {
-//     e.preventDefault();
-//     $('body,html').animate({
-//       scrollTop: $(this.hash).offset().top
-//     }, 1000 );
-//   });
-  
-//   // Active link switching
-//   $(window).scroll(function() {
-//     var scrollbarLocation = $(this).scrollTop();
+
+  // Set the modal image source to the one of the clicked image
+  $('.portfolio-item').on('click', function (e) {
+    let imageElement = $(e.currentTarget).find('img');
+    let imageSource = imageElement.attr('src');
+    $('.modal-body img').attr('src', imageSource);
+  });
+
+  // Ajax call
+  $.ajax({
+    url : 'https://reqres.in/api/unknown',
+    data : '',
+    type : 'GET',
+    dataType : 'json'
+  }).done(function (json) {
+
+    if (json.data !== undefined) { // Check if the data is there
+      for (let i = 0; i < json.data.length; i++) { // Iterate over the array
+        let newOption = $('<option>') // Create a shadow element
+            .attr('value', json.data[i].color)
+            .text(json.data[i].name);
+
+        newOption.appendTo($('#colours')); // Add the element to the select box
+      }  
+    }
     
-//     scrollLink.each(function() {
-      
-//       var sectionOffset = $(this.hash).offset().top - 20;
-      
-//       if ( sectionOffset <= scrollbarLocation ) {
-//         $(this).parent().addClass('active');
-//         $(this).parent().siblings().removeClass('active');
-//       }
-//     })
-    
-//   })
-  
-// })
+    $('#colours').on('change', function () { // Handle the selection event
+      $('#message').css('background-color', $('#colours').val() );
+    });
+  });
+
+
+}); 
